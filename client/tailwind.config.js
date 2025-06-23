08e0ed9d5219f726d149af36743f423dc30731a3
Sanjy-KV <sanjayk200229@gmail.com>
2025-06-24 03:53:26 +0530

    issue fixed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  // Add these if you're using App Router:
+  "./app/**/*.{js,ts,jsx,tsx}"
+];
+export const theme = { /* ... */ };
+export const plugins = [];
