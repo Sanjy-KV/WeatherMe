91b3de3dbff20c1e8a0c7b3ee956de96951b6dac
Sanjy-KV <sanjayk200229@gmail.com>
2025-06-25 18:30:40 +0530

    changed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-  // Add these if you're using App Router:
-  "./app/**/*.{js,ts,jsx,tsx}"
-];
-export const theme = { /* ... */ };
-export const plugins = [];
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
